90bd4a8350c882707cec7e4d4667cddfd995c29d
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-09-26 08:57:32 +0100

    add event bus name

--- monitoring.tf
+++ monitoring.tf
@@ -281,6 +281,7 @@ resource "aws_cloudwatch_event_bus" "file_checks" {
 resource "aws_cloudwatch_event_rule" "file_size_check_failed" {
   name          = "file_size_check_failed_rule"
   description   = "checks that file size is within a certain range"
+  event_bus_name = aws_cloudwatch_event_bus.file_checks.name
   event_pattern = <<EOF
 {
   "source": ["${local.event_source}}"],
