d14e68271d31ab1f5c751e2de5c459f7bc39cf64
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-10-05 15:38:40 +0100

    remove comma

--- iam_additional.tf
+++ iam_additional.tf
@@ -367,7 +367,7 @@ data "aws_iam_policy_document" "ch_events" {
     ]
 
     resources = concat([
-      aws_cloudwatch_event_rule.ch_started.arn, ,aws_cloudwatch_event_rule.ch_success.arn,
+      aws_cloudwatch_event_rule.ch_started.arn, aws_cloudwatch_event_rule.ch_success.arn,
       aws_cloudwatch_event_rule.ch_terminated_with_errors_rule.arn, aws_cloudwatch_event_rule.delta_file_size_check_failed.arn, aws_cloudwatch_event_rule.file_size_check_failed.arn,
       aws_cloudwatch_event_rule.file_landed.arn,
       aws_cloudwatch_metric_alarm.ch_started.arn, aws_cloudwatch_metric_alarm.ch_success.arn,
