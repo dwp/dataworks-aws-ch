3c88fd3c3a5e89e7fc77bd10fa081029f770c563
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-11-22 09:19:22 +0000

    bootstrap action 7 failing on one of the instances. updating script

--- bootstrap_actions/steps-setup.sh
+++ bootstrap_actions/steps-setup.sh
@@ -8,13 +8,13 @@ set -euo pipefail
         log_ch_message "$1" "steps-setup.sh" "$$" "Running as: $USER"
     }
 
-    log_wrapper_message "Moving maria db jar to spark jars folder"
-    sudo mkdir -p /usr/lib/spark/jars/
-    sudo cp /usr/share/java/mariadb-connector-java.jar /usr/lib/spark/jars/
+#    log_wrapper_message "Moving maria db jar to spark jars folder"
+#    sudo mkdir -p /usr/lib/spark/jars/
+#    sudo cp /usr/share/java/mariadb-connector-java.jar /usr/lib/spark/jars/
 
     log_wrapper_message "Setting up EMR steps folder"
     sudo mkdir -p /opt/emr/steps
-    sudo chown hadoop:hadoop /opt/emr/steps
+    sudo chown -R hadoop:hadoop /opt/emr/steps
 
     log_wrapper_message "Creating init py file"
     touch /opt/emr/steps/__init__.py
@@ -24,7 +24,7 @@ set -euo pipefail
     aws s3 cp "${etl_e2e}" /opt/emr/steps/e2e.py
     aws s3 cp "${etl_e2e_conf}" /opt/emr/steps/e2e_test_conf.tpl
     aws s3 cp "${etl_conf}" /opt/emr/steps/conf.tpl
-    sudo chmod --recursive a+rx /opt/emr/steps
+    sudo chmod --recursive 777 /opt/emr/steps
 
     log_wrapper_message "Scripts in steps folder:"
     ls  /opt/emr/steps
