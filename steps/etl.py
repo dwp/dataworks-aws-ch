71755ce8ce06bbeb3f459d7f3aa51e791f84ccd2
camillascf <camilla.scuffi@credera.co.uk>
2023-02-14 14:06:08 +0000

    remove delete csv as will be added as bucket retention policy

--- steps/etl.py
+++ steps/etl.py
@@ -473,17 +473,6 @@ def unzip_file_in_loco(source_bucket, prefix, zip_file, csv_file):
         sys.exit(-1)
 
 
-def delete_csv_file(bucket, key):
-    try:
-        logger.info(f"deleting {key} from {bucket}")
-        s3 = boto3.resource('s3')
-        s3.Object(bucket, key).delete()
-
-    except Exception as ex:
-        logger.error(f"Failed to delete file. {ex}")
-        sys.exit(-1)
-
-
 
 if __name__ == "__main__":
     args = all_args()
@@ -511,7 +500,6 @@ if __name__ == "__main__":
     else:
         new_df = add_partitioning_column(extraction_df, day, partitioning_column)
     write_parquet(new_df, destination, partitioning_column)
-    delete_csv_file(source_bucket, os.path.join(args['args']['source_prefix'], new_file_csv))
     db = args['args']['db_name']
     tbl = args['args']['table_name']
     recreate_hive_table(new_df, destination, db, tbl, spark, partitioning_column)
