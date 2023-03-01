c55893295ab1b9dc381823759cae5e29f9e076f9
camillascuffi <>
2023-03-01 16:39:24 +0000

    trigger lambda

--- emr_launcher.tf
+++ emr_launcher.tf
@@ -22,26 +22,25 @@ resource "aws_lambda_function" "ch_emr_launcher" {
   }
 }
 
-resource "aws_sns_topic_subscription" "trigger_ch" {
-  topic_arn = aws_sns_topic.trigger_ch_sns.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.ch_emr_launcher.arn
-}
-
-resource "aws_lambda_permission" "ch_emr_launcher_subscription" {
-  statement_id  = "TriggerCHProcess"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ch_emr_launcher.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.trigger_ch_sns.arn
-}
-
-
+//resource "aws_sns_topic_subscription" "trigger_ch" {
+//  topic_arn = aws_sns_topic.trigger_ch_sns.arn
+//  protocol  = "lambda"
+//  endpoint  = aws_lambda_function.ch_emr_launcher.arn
+//}
+//
+//resource "aws_lambda_permission" "ch_emr_launcher_subscription" {
+//  statement_id  = "TriggerCHProcess"
+//  action        = "lambda:InvokeFunction"
+//  function_name = aws_lambda_function.ch_emr_launcher.function_name
+//  principal     = "sns.amazonaws.com"
+//  source_arn    = aws_sns_topic.trigger_ch_sns.arn
+//}
+//
 
 resource "aws_cloudwatch_event_rule" "every_month" {
     name = "every-month"
     description = "Fires every month"
-    schedule_expression = "cron(55 * * * ? *)"
+    schedule_expression = "cron(45 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "every_month" {
