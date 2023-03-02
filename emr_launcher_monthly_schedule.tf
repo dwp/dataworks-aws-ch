6ff61fc2c578018af4a903d38e3b4a297a5f7dc7
camillascuffi <>
2023-03-02 11:02:13 +0000

    test in dev

--- emr_launcher_monthly_schedule.tf
+++ emr_launcher_monthly_schedule.tf
@@ -1,12 +1,14 @@
 resource "aws_cloudwatch_event_rule" "every_month" {
-    count = local.environment == "production" ? 1 : 0
+    count = local.environment == "development" ? 1 : 0
     name = "every-month"
     description = "Fires every month"
-    schedule_expression = "cron(30 8 5 * ? *)"
+    schedule_expression = "cron(9 11 2 * ? *)"
+
+//    schedule_expression = "cron(30 8 5 * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "every_month" {
-    count = local.environment == "production" ? 1 : 0
+    count = local.environment == "development" ? 1 : 0
     rule = aws_cloudwatch_event_rule.every_month[0].name
     arn = aws_lambda_function.ch_emr_launcher.arn
     target_id = "lambdaCHtriggerTarget"
@@ -20,7 +22,7 @@ resource "aws_cloudwatch_event_target" "every_month" {
 }
 
 resource "aws_lambda_permission" "every_month" {
-    count = local.environment == "production" ? 1 : 0
+    count = local.environment == "development" ? 1 : 0
     statement_id = "AllowExecutionFromCloudWatch"
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.ch_emr_launcher.function_name
